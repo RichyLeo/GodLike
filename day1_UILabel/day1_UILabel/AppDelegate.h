ca281b17a27f789ed8bf399768708a544186315c
lipeichi <godlikelpc@126.com>
2015-12-22 14:42:39 +0800

    rebwbvew

---
+++ day1_UILabel/day1_UILabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  day1_UILabel
+//
+//  Created by 神丶宁静致远 on 15/8/24.
+//  Copyright (c) 2015年 LPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ day1_UILabel/day1_UILabelTests/day1_UILabelTests.m
@@ -0,0 +1,34 @@
+//
+//  day1_UILabelTests.m
+//  day1_UILabelTests
+//
+//  Created by 神丶宁静致远 on 15/8/24.
+//  Copyright (c) 2015年 LPC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface day1_UILabelTests : XCTestCase
+
+@end
+
+@implementation day1_UILabelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
